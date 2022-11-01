c92d2bc5e384170b8c940db27529d2c5a6184865
KooYS <tlfkthsl42@gmail.com>
2022-11-01 11:11:16 +0900

    Chore: add custom spacing, screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
 const colors = require('tailwindcss/colors');
 
+const pxToRem = (px, base = 16) => `${px / base}rem`;
+
+const range = (m, n) => {
+  return Array.from(Array(n + 1 - m).keys())
+    .map((v) => v + m)
+    .map((v) => v);
+};
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -12,6 +20,12 @@ module.exports = {
       mobile: '360px', // @media (min-width: 360px)
       foldable: '523px', // @media (min-width: 523px)
       tablet: '768px', // @media (min-width: 768px)
+    },
+    spacing: {
+      ...range(1, 100).reduce((acc, px) => {
+        acc[`${px}pxr`] = pxToRem(px);
+        return acc;
+      }, {}),
     },
     extend: {},
   },
